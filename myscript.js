0b41b9be148de24b19ae3d8884e9605e138484b6
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-01-29 18:18:34 +0100

    completato script

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 function priceCalculator() {
+    
     document.getElementById("price-container").style.display = "block";
 
     const nKm = parseInt(prompt("Quanti km vuoi percorrere?"));
@@ -8,11 +9,25 @@ function priceCalculator() {
         return;
     }
 
-
     const eta = parseInt(prompt("Quanti anni ha il passeggero?"));
 
     if (isNaN(eta)) {
         document.getElementById("output").innerHTML = "Il valore di età da te inserito è scorretto. Ricomincia da capo.";
         return;
     }
+
+    let prezzo = nKm * 0.21;
+
+    if (eta < 18) {
+        prezzo -= prezzo * 20 / 100;
+    }
+
+    if (eta >= 65) {
+        prezzo -= prezzo * 40 / 100;
+    }
+
+    prezzo = (Math.round(prezzo * 100) / 100).toFixed(2);
+
+    document.getElementById("output").innerHTML = prezzo + "€";
+
 }
